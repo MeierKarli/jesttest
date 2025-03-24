bdee924ba055a7bc63816f41021a162c34f5909e
Karl Meier <korl_meia@hotmail.com>
2025-03-24 08:26:23 +0100

    updated URL in config

--- config/base.config.ts
+++ config/base.config.ts
@@ -1,5 +1,6 @@
 export default {
-  baseUrl: 'https://sdetunicorns.com/api/test',
+  //baseUrl: 'https://sdetunicorns.com/api/test',
+  baseUrl: 'https://practice-react.sdetunicorns.com',
   email: "mod@mail.com",
   password: "Modpass123!"
 }
